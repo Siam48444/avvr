b0dfc0f8d4dd42b6183eec788c2dbedbee791e90
Siam <135020238+Siam48444@users.noreply.github.com>
2023-11-26 18:36:31 +0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+function HeaderColorChange() {
+    const Header = document.querySelector("header");
+    var HeaderBottom = Header.getBoundingClientRect().bottom;
+    
+    const Work = document.querySelector(".works_section");
+    var WorkTop = Work.getBoundingClientRect().top;
+    var WorkBottom = Work.getBoundingClientRect().bottom;
+    
+    if(WorkTop <= HeaderBottom) {
+        Header.classList.add("header_color");
+    } else {
+        Header.classList.remove("header_color")
+    };
+};
+window.addEventListener("scroll", HeaderColorChange)
+
+
+
+
+console.log();
