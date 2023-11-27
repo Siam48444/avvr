e6c7fa5b388c71f08f9b4593f814c0b8ba54aca4
Siam <135020238+Siam48444@users.noreply.github.com>
2023-11-27 07:07:33 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
+// Header color changing.
 function HeaderColorChange() {
     const Header = document.querySelector("header");
+    const HeaderElements = document.getElementsByClassName("color_change_elements");
+    const HeaderSearchBox = document.querySelector(".search_button");
+    const HeaderSVG = document.querySelector(".search_svg");
     var HeaderBottom = Header.getBoundingClientRect().bottom;
     
     const Work = document.querySelector(".works_section");
     var WorkTop = Work.getBoundingClientRect().top;
     var WorkBottom = Work.getBoundingClientRect().bottom;
     
-    if(WorkTop <= HeaderBottom) {
-        Header.classList.add("header_color");
-    } else {
-        Header.classList.remove("header_color")
+    if(WorkTop < HeaderBottom) {
+        Header.style.backgroundColor = "var(--color-white)";
+        HeaderSVG.style.fill = "var(--color-black)";
+        for(let i = 0; i < HeaderElements.length; i++) {
+            HeaderElements[i].style.color = "var(--color-black)";
+        };
+        // Search Hover.    
+        HeaderSearchBox.addEventListener("mouseenter", ()=> {
+            HeaderSVG.style.fill = "var(--color-white)";
+        });
+    
+        HeaderSearchBox.addEventListener("mouseleave", ()=> {
+            HeaderSVG.style.fill = "var(--color-black)";
+        });
+    } if(WorkTop > HeaderBottom) {
+        Header.style.backgroundColor = "var(--color-green-dark)";
+        HeaderSVG.style.fill = "var(--color-white)";
+        for(let i = 0; i < HeaderElements.length; i++) {
+            HeaderElements[i].style.color = "var(--color-white)";
+        };
+        // Search Hover.        
+        HeaderSearchBox.addEventListener("mouseenter", ()=> {
+            HeaderSVG.style.fill = "var(--color-white)";
+        });
+    
+        HeaderSearchBox.addEventListener("mouseleave", ()=> {
+            HeaderSVG.style.fill = "var(--color-white)";
+        });
+    } if(WorkBottom < HeaderBottom) {
+        Header.style.backgroundColor = "var(--color-green-dark)";
+        HeaderSVG.style.fill = "var(--color-white)";
+        for(let i = 0; i < HeaderElements.length; i++) {
+            HeaderElements[i].style.color = "var(--color-white)";
+        };
+        // Search Hover.        
+        HeaderSearchBox.addEventListener("mouseenter", ()=> {
+            HeaderSVG.style.fill = "var(--color-white)";
+        });
+    
+        HeaderSearchBox.addEventListener("mouseleave", ()=> {
+            HeaderSVG.style.fill = "var(--color-white)";
+        });
     };
 };
 window.addEventListener("scroll", HeaderColorChange)
