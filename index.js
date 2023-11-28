50b75607d9bbac2ed5ded2da00dddcc7f8d82f10
Siam <135020238+Siam48444@users.noreply.github.com>
2023-11-28 17:30:14 +0600

    Update index.js

--- index.js
+++ index.js
@@ -25,36 +25,4 @@ function HeaderColorChange() {
         };
     };
 };
-window.addEventListener("scroll", HeaderColorChange)
-
-
-// Custom mouse cursor.
-function TrackCursor(e) {
-    const CustomCursor = document.querySelector(".cursor");
-    var w = CustomCursor.clientWidth;
-    var h = CustomCursor.clientHeight;
-    
-    CustomCursor.style.transform = `translate(${e.pageX - w / 2}px, ${e.pageY - h / 2}px)`
-
-
-    document.addEventListener("mouseenter", ()=> {
-        CustomCursor.style.display = "block";
-    });
-
-    document.addEventListener("mouseleave", ()=> {
-        CustomCursor.style.display = "none";
-    });
-    
-
-    const DotGoneElements = document.getElementsByClassName("dot_disappears");
-    for(let i = 0; i < DotGoneElements.length; i++) {
-        DotGoneElements[i].addEventListener("mouseenter", ()=> {
-            CustomCursor.style.display = "none";
-        });
-
-        DotGoneElements[i].addEventListener("mouseleave", ()=> {
-            CustomCursor.style.display = "block";
-        });
-    };
-};
-window.addEventListener("mousemove", TrackCursor);
+window.addEventListener("scroll", HeaderColorChange);
